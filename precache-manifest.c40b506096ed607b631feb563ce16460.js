f1f4bbd895423e36c4a3e9078e2d42b74bcee39f
panda1920 <15102478+panda1920@users.noreply.github.com>
2019-10-10 02:14:20 +0900

    Updates

---
+++ precache-manifest.c40b506096ed607b631feb563ce16460.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dede5520e08340c9d1520cdec0c40caf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8f96fee44741cd2529bc",
+    "url": "/monsters-rolodex/static/css/main.d3c58058.chunk.css"
+  },
+  {
+    "revision": "2b1c418810eda8255f8f",
+    "url": "/monsters-rolodex/static/js/2.a24ef447.chunk.js"
+  },
+  {
+    "revision": "8f96fee44741cd2529bc",
+    "url": "/monsters-rolodex/static/js/main.f8f0d672.chunk.js"
+  },
+  {
+    "revision": "61e64e548c9076aea34b",
+    "url": "/monsters-rolodex/static/js/runtime-main.c620570a.js"
+  }
+]);
